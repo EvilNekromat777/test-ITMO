c3baed1e045a21967d623d70dc0d3d25ace4872c
Evgeniya Kireeva <evil-nekromat777@mail.ru>
2021-03-24 11:37:48 +0300

    feat: bunner: slider + adaptive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
+//Меню бургер
 $(document).ready(function() {
 	$('.header__burger').click(function(event) {
 		$('.header__burger,.header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+});
+
+//======================================================>
+
+//Слайдер на Bootstrap5
+const myCarousel = document.querySelector('#carousel');
+const carousel = new bootstrap.Carousel(myCarousel, { 
+  interval: false,
 });
