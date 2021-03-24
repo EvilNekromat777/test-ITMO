bf01e75cc5965600d0a3bb2c5f0cdf3af92cce8f
Evgeniya Kireeva <evil-nekromat777@mail.ru>
2021-03-24 14:13:56 +0300

    feat: block disciplines + accordion

--- js/script.js
+++ js/script.js
@@ -13,3 +13,19 @@ const myCarousel = document.querySelector('#carousel');
 const carousel = new bootstrap.Carousel(myCarousel, { 
   interval: false,
 });
+
+//======================================================>
+
+//Accordion
+$('.accordion').each(function () {
+	let $accordian = $(this);
+	$accordian.find('.accordion-head').on('click', function () {
+		  $(this).parent().find(".accordion-head").removeClass('open close');
+		  $(this).removeClass('open').addClass('close');
+		 $accordian.find('.accordion-body').slideUp();
+		 if (!$(this).next().is(':visible')) {
+				$(this).removeClass('close').addClass('open');
+			  $(this).next().slideDown();
+		 }
+	});
+});
